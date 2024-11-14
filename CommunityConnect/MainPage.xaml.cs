d16bc8eac062e1aa78ebd2bc887f5fc5d4c41688
Tshikwetamakole <105504810+Tshikwetamakole@users.noreply.github.com>
2024-11-14 14:22:56 +0200

    charley

--- CommunityConnect/MainPage.xaml.cs
+++ CommunityConnect/MainPage.xaml.cs
@@ -1,25 +1,29 @@
-﻿namespace CommunityConnect
+﻿using System.Collections.ObjectModel;
+using CommunityConnect.model;
+
+namespace CommunityConnect
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        public ObservableCollection<CommunityConnect.model.MenuItem> MenuItems { get; set; }
+
 
         public MainPage()
         {
             InitializeComponent();
-        }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
+            MenuItems = new ObservableCollection<CommunityConnect.model.MenuItem>
+{
+    new CommunityConnect.model.MenuItem { Name = "Real-Time Incident Reporting", Icon = "incident_report_icon.png" },
+    new CommunityConnect.model.MenuItem { Name = "Real-Time Alerts (Sebatakgomo)", Icon = "alerts_icon.png" },
+    new CommunityConnect.model.MenuItem { Name = "Incident Feed/Map View", Icon = "map_view_icon.png" },
+    new CommunityConnect.model.MenuItem { Name = "Community Collaboration", Icon = "collaboration_icon.png" },
+    new CommunityConnect.model.MenuItem { Name = "People of Interest", Icon = "missing_people_icon.png" },
+    new CommunityConnect.model.MenuItem { Name = "User Profile and Contributions", Icon = "user_profile_icon.png" }
+};
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            BindingContext = this;
         }
     }
-
 }
