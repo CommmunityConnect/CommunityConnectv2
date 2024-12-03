297da9a96a0b5dd356ced8eb6ac67c47afbe55c2
KOKETSO23 <koketsosefefe@gmail.com>
2024-12-03 11:52:27 +0200

    commit

--- CommunityConnect/Pages/Profile.xaml.cs
+++ CommunityConnect/Pages/Profile.xaml.cs
@@ -1,9 +1,17 @@
-namespace CommunityConnect.Pages;
-
-public partial class Profile : ContentPage
+namespace CommunityConnect.Pages
 {
-	public Profile()
-	{
-		InitializeComponent();
-	}
+    public partial class Profile : ContentPage
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+
+        // This method should not be inside the constructor and should be defined outside
+        private async void OnLogoutTapped(object sender, EventArgs e)
+        {
+            // Navigate to the sign-in page
+            await Navigation.PushAsync(new signin());
+        }
+    }
 }
