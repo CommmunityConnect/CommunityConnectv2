eb7c1c7c1e76958f33588bfc26390d41f17cee13
KOKETSO23 <koketsosefefe@gmail.com>
2024-12-12 17:09:21 +0200

    commit

--- CommunityConnect/Pages/signup.xaml.cs
+++ CommunityConnect/Pages/signup.xaml.cs
@@ -1,3 +1,4 @@
+using CommunityConnect.ViewModel;
 using Microsoft.Maui.Controls;
 
 namespace CommunityConnect.Pages
@@ -7,35 +8,32 @@ namespace CommunityConnect.Pages
         public signup()
         {
             InitializeComponent();
+            BindingContext = new SignupViewModel();
+        }
+
+        private void OnTappedNavigateToLogin(object sender, EventArgs e)
+        {
+            // Navigate to login page
+            Shell.Current.GoToAsync("//LoginPage");
+        }
+
+        private void OnShowPasswordCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            passwordEntry.IsPassword = !e.Value;
         }
 
         private async void OnSignUpClicked(object sender, EventArgs e)
         {
-            // Validate fields
-            if (string.IsNullOrWhiteSpace(nameEntry.Text) ||
-                string.IsNullOrWhiteSpace(emailEntry.Text) ||
-                string.IsNullOrWhiteSpace(passwordEntry.Text))
-            {
-                await DisplayAlert("Error", "Please fill in all fields before signing up.", "OK");
-                return;
-            }
+            var userType = userTypePicker.SelectedItem as string;
+            var username = nameEntry.Text;
+            var email = emailEntry.Text;
+            var password = passwordEntry.Text;
 
-            // Simulate successful account creation
-            await DisplayAlert("Success", "Account created successfully!", "OK");
+            // Perform sign-up logic here
 
-            // Redirect to the login page
-            await Shell.Current.GoToAsync("signin");
-        }
-
-        // Event handler for when the "Show Password" checkbox is checked/unchecked
-        private void OnShowPasswordCheckedChanged(object sender, CheckedChangedEventArgs e)
-        {
-            // Toggle the IsPassword property of the passwordEntry
-            passwordEntry.IsPassword = !e.Value;
-        }
-        private async void OnTappedNavigateToLogin(object sender, TappedEventArgs e)
-        {
-            await Navigation.PushAsync(new signin());
+            await DisplayAlert("Success", $"Signed up as {userType}", "OK");
+            await Shell.Current.GoToAsync("//MainPage");
         }
     }
 }
+
---
+++ CommunityConnect/ViewModel/GeneralDiscussionsViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+
+namespace CommunityConnect.ViewModel
+{
+    public class GeneralDiscussionsViewModel : INotifyPropertyChanged
+    {
+        private string newPostContent;
+        private string newCommentContent;
+
+        public string NewPostContent
+        {
+            get => newPostContent;
+            set
+            {
+                if (newPostContent != value)
+                {
+                    newPostContent = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string NewCommentContent
+        {
+            get => newCommentContent;
+            set
+            {
+                if (newCommentContent != value)
+                {
+                    newCommentContent = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableCollection<Post> Posts { get; } = new ObservableCollection<Post>();
+
+        public ICommand PostCommand { get; }
+        public ICommand CommentCommand { get; }
+
+        public GeneralDiscussionsViewModel()
+        {
+            PostCommand = new Command(OnPost);
+            CommentCommand = new Command<Post>(OnComment);
+        }
+
+        private void OnPost()
+        {
+            if (!string.IsNullOrWhiteSpace(NewPostContent))
+            {
+                Posts.Add(new Post { Content = NewPostContent });
+                NewPostContent = string.Empty;
+            }
+        }
+
+        private void OnComment(Post post)
+        {
+            if (post != null && !string.IsNullOrWhiteSpace(NewCommentContent))
+            {
+                post.Comments.Add(new Comment { Content = NewCommentContent });
+                NewCommentContent = string.Empty;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class Post
+    {
+        public string Content { get; set; }
+        public ObservableCollection<Comment> Comments { get; } = new ObservableCollection<Comment>();
+        public string NewCommentContent { get; set; }
+    }
+
+    public class Comment
+    {
+        public string Content { get; set; }
+    }
+}
+
