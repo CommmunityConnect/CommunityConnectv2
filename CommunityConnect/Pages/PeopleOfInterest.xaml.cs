6fb190cfe280c6759020f8b6949cc09ede27e598
Reginah201 <reginahmakgomo2@gmail.com>
2024-12-12 14:23:16 +0200

    commit

--- CommunityConnect/Pages/PeopleOfInterest.xaml.cs
+++ CommunityConnect/Pages/PeopleOfInterest.xaml.cs
@@ -5,5 +5,27 @@ public partial class PeopleOfInterest : ContentPage
 	public PeopleOfInterest()
 	{
 		InitializeComponent();
-	}
+        AddGestureRecognizers();
+
+    }
+    private void AddGestureRecognizers()
+    {
+        // Missing Persons
+        MissingPersonsFrame.GestureRecognizers.Add(new TapGestureRecognizer
+        {
+            Command = new Command(() => NavigateToPage("MissingPersons"))
+        });
+
+        // Wanted Persons
+        WantedPersonsFrame.GestureRecognizers.Add(new TapGestureRecognizer
+        {
+            Command = new Command(() => NavigateToPage("WantedPersons"))
+        });
+    }
+
+
+    private async void NavigateToPage(string route)
+    {
+        await Shell.Current.GoToAsync(route);
+    }
 }
